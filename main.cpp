3f448d7b7df5e1354134f73ce8605cc8c79660a6
tbg <tellepsarecool@gmail.com>
2021-09-30 20:00:43 +1000

    Added living neighbour count

--- main.cpp
+++ main.cpp
@@ -34,25 +34,48 @@ void random_state(int** board_state, const int B_WIDTH, const int B_HEIGHT){
     }
 }
 
+int next_board_state(int** board_state, const int B_WIDTH, const int B_HEIGHT){
+    int count = 0;
+    for (int x = 0; x<B_WIDTH; x++){
+            for (int y = 0; y<B_HEIGHT; y++){
+               for(int row = -1; row<= 1; row++){
+                 for(int col = -1; col<= 1; col++){
+                     int current_x = x + row;
+                     int current_y = y + col;
+                     if (current_x >= 0 && current_x < B_WIDTH && current_y >= 0 && current_y < B_HEIGHT){
+                        if(board_state[current_x][current_y] == 1){
+                            count++;
+                        }
+                     }
+                 }
+             }
+         }
+    }
+    return count;
+}
+
+
 void print_board(int** board_state, const int B_WIDTH, const int B_HEIGHT){
     for (int x = 0; x<B_WIDTH; x++){
             for (int y = 0; y<B_HEIGHT; y++){
-             cout << board_state[x][y];
+             if(board_state[x][y] == 0){cout << ' ';}
+             else{cout << '#';}
             }
+            cout << endl;
     }
 }
 
 int main()
 {
     srand(time(NULL));
-    const int B_WIDTH = 3;
-    const int B_HEIGHT = 4;
+    const int B_WIDTH = 10;
+    const int B_HEIGHT = 10;
 
-    // Create 2D array by passing a pointer to a pointer
+    // Create 2D pointer array
 
     int** board_state = allocate_board(B_WIDTH, B_HEIGHT);
 
-    // Initialise whole array with value 0 for memory safety
+    // Initialise whole array with value 0
 
     dead_state(board_state, B_WIDTH, B_HEIGHT);
 
@@ -64,6 +87,11 @@ int main()
     random_state(board_state, B_WIDTH, B_HEIGHT);
 
     print_board(board_state, B_WIDTH, B_HEIGHT);
+
+    cout << "=================================================" << endl;
+    //Counts the living neighbours of every alive cell
+    cout << next_board_state(board_state, B_WIDTH, B_HEIGHT);
+
 
     // Deallocate memory
 
